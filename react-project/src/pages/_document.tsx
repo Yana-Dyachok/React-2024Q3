097cd1fd697b5d8d31e652e906f64f6b78b648e6
Yana <annetart1994@gmail.com>
2024-08-02 21:34:03 +0300

    refactor: change code in  document, add favicon

--- react-project/src/pages/_document.tsx
+++ react-project/src/pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-}
+const NextDocument = () => (
+  <Html>
+    <Head>
+      <meta charSet="UTF-8" />
+      <meta name="description" content="react project with next.js" />
+      <link rel="icon" href="/favicon-doc.ico" type="image/ico" sizes="32x32" />
+    </Head>
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
+export default NextDocument;

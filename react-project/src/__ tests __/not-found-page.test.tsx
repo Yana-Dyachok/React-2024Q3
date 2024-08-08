b10a17601eb758a8c7ca40474e576519300c36ba
Yana <annetart1994@gmail.com>
2024-08-08 19:06:31 +0300

    feat: add not found page

--- react-project/src/__ tests __/not-found-page.test.tsx
+++ react-project/src/__ tests __/not-found-page.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import NotFoundPage from '@/app/error';
+import NotFoundPage from '@/app/not-found';
 import { useTheme } from '../theme-context/theme-context';
 import { ThemeProvider } from '../theme-context/theme-provider';
 

5e514aa9b362c566fce40af499ee2dd69f607b05
Yana <annetart1994@gmail.com>
2024-08-11 18:38:04 +0300

    refactore: change code in root

--- react-project/.next/server/server-reference-manifest.js
+++
@@ -1,2 +0,0 @@
-self.__RSC_SERVER_MANIFEST =
-  '{\n  "node": {},\n  "edge": {},\n  "encryptionKey": "Q9ETS1PWDk83L8v7irsThDXCgpfP1RqiIKiuayzNI24="\n}';
--- react-project/app/root.tsx
+++ react-project/app/root.tsx
@@ -24,7 +24,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <body>
         <ThemeProvider>
           <StoreProvider>
-            {children}
+            <Suspense fallback={<Loading />}>{children}</Suspense>
             <ScrollRestoration />
             <Scripts />
           </StoreProvider>
@@ -37,9 +37,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 export default function App() {
   return (
     <Layout>
-      <Suspense fallback={<Loading />}>
-        <Outlet />
-      </Suspense>
+      <Outlet />
     </Layout>
   );
 }
